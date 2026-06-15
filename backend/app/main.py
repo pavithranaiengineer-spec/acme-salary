3994c288b06867858b3eccc495475c553b5c2721
Pavithran Ai <pavithranaiengineer@gmail.com>
2026-06-15 14:49:18 +0530

    fix: cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://acme-salary-wxcz.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
